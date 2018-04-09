afe6f574ffccd5488efef93d278ac8f41ac564e0
chatellierclement <chatellierclement44@gmail.com>
2018-04-09 12:25:52 +0200

    DAL

--- app/src/main/java/com/example/administrateur/projet/Bouchon/LoginBouchon.java
+++ app/src/main/java/com/example/administrateur/projet/Bouchon/LoginBouchon.java
@@ -2,21 +2,30 @@ package com.example.administrateur.projet.Bouchon;
 
 import com.example.administrateur.projet.BO.Utilisateur;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrateur on 09/04/2018.
  */
 
 public class LoginBouchon {
 
+    private List<Utilisateur> utilisateurList = new ArrayList<>();
+
     public LoginBouchon() {
     }
 
-    public Utilisateur getOneUser(Utilisateur utilisateur) {
-        Utilisateur utilisateurBouchon = new Utilisateur();
-        utilisateurBouchon.setNom("admin");
-        utilisateurBouchon.setMotDePasse("admin");
+    private List<Utilisateur> getUtilisateurList() {
+        utilisateurList.add(new Utilisateur("Jean", "Jean"));
+        utilisateurList.add(new Utilisateur("Admin", "Admin"));
+        return utilisateurList;
+    }
 
-        if (utilisateur == utilisateurBouchon) {
+    public Utilisateur getOneUser(Utilisateur utilisateur) {
+        List<Utilisateur> utilisateurList = getUtilisateurList();
+
+        if (utilisateurList.contains(utilisateur)) {
             return utilisateur;
         }
         return null;
