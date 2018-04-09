91d1eb49409c92bcc2a0c1dea31b4ab858ce1e36
wtmaxim <maxime.derame@hotmail.fr>
2018-04-09 15:32:05 +0200

    Model

--- app/src/main/java/com/example/administrateur/projet/BO/Agence.java
+++ app/src/main/java/com/example/administrateur/projet/BO/Agence.java
@@ -1,8 +1,70 @@
 package com.example.administrateur.projet.BO;
 
-/**
- * Created by Administrateur on 09/04/2018.
- */
-
 public class Agence {
+    private int id;
+    private String raisonSocial;
+    private String siret;
+    private String voie;
+    private String cp;
+    private String vile;
+
+    public Agence() {
+    }
+
+    public Agence(int id, String raisonSocial, String siret, String voie, String cp, String vile) {
+        this.id = id;
+        this.raisonSocial = raisonSocial;
+        this.siret = siret;
+        this.voie = voie;
+        this.cp = cp;
+        this.vile = vile;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRaisonSocial() {
+        return raisonSocial;
+    }
+
+    public void setRaisonSocial(String raisonSocial) {
+        this.raisonSocial = raisonSocial;
+    }
+
+    public String getSiret() {
+        return siret;
+    }
+
+    public void setSiret(String siret) {
+        this.siret = siret;
+    }
+
+    public String getVoie() {
+        return voie;
+    }
+
+    public void setVoie(String voie) {
+        this.voie = voie;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+
+    public String getVile() {
+        return vile;
+    }
+
+    public void setVile(String vile) {
+        this.vile = vile;
+    }
 }
--- app/src/main/java/com/example/administrateur/projet/BO/Location.java
+++ app/src/main/java/com/example/administrateur/projet/BO/Location.java
@@ -1,8 +1,66 @@
 package com.example.administrateur.projet.BO;
 
+import java.util.Date;
+
 /**
  * Created by Administrateur on 09/04/2018.
  */
 
 public class Location {
+    private int id;
+    private Vehicule vehicule;
+    private Date dateDebut;
+    private Date dateFin;
+    private float tarif;
+
+    public Location() {
+    }
+
+    public Location(int id, Vehicule vehicule, Date dateDebut, Date dateFin, float tarif) {
+        this.id = id;
+        this.vehicule = vehicule;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.tarif = tarif;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Vehicule getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(Vehicule vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public float getTarif() {
+        return tarif;
+    }
+
+    public void setTarif(float tarif) {
+        this.tarif = tarif;
+    }
 }
