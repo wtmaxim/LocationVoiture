55e2955958ce0fef379711266e4ef3d9f34bd34c
chatellierclement <chatellierclement44@gmail.com>
2018-04-09 16:48:21 +0200

    dal

--- app/src/main/java/com/example/administrateur/projet/Bouchon/LocationBouchon.java
+++ app/src/main/java/com/example/administrateur/projet/Bouchon/LocationBouchon.java
@@ -1,14 +1,36 @@
 package com.example.administrateur.projet.Bouchon;
 
 import com.example.administrateur.projet.BO.Vehicule;
+import com.example.administrateur.projet.DAL.ILocation;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LocationBouchon {
+public class LocationBouchon implements ILocation {
+
     private static List<Vehicule> vehicules = new ArrayList<>();
 
-    private static List<Vehicule> getVehicules() {
+    @Override
+    public void louer(Vehicule vehicule) {
+        vehicule.setEstLoue(true);
+    }
+
+    @Override
+    public void rendre(Vehicule vehicule) {
+        vehicule.setEstLoue(false);
+    }
+
+    @Override
+    public List<Vehicule> getVehiculesLoues() {
+        return getVehicules(true);
+    }
+
+    @Override
+    public List<Vehicule> getVehiculesDisponibles() {
+        return getVehicules(false);
+    }
+
+    private List<Vehicule> getDataVehicules() {
         vehicules.add(new Vehicule(1, "Voiture 1", 4, 6, 2, 30, 100, false));
         vehicules.add(new Vehicule(2, "Voiture 2", 2, 6, 2, 30, 100, false));
         vehicules.add(new Vehicule(3, "Voiture 3", 1, 6, 2, 30, 100, true));
@@ -22,6 +44,12 @@ public class LocationBouchon {
         return vehicules;
     }
 
-
-
+    private List<Vehicule> getVehicules(Boolean estLoue) {
+        for (Vehicule v : getDataVehicules()) {
+            if (v.isEstLoue() == estLoue) {
+                vehicules.remove(v);
+            }
+        }
+        return vehicules;
+    }
 }
--- app/src/main/java/com/example/administrateur/projet/DAL/ILocation.java
+++ app/src/main/java/com/example/administrateur/projet/DAL/ILocation.java
@@ -1,8 +1,17 @@
 package com.example.administrateur.projet.DAL;
 
+import com.example.administrateur.projet.BO.Vehicule;
+
+import java.util.List;
+
 /**
  * Created by Administrateur on 09/04/2018.
  */
 
-public class ILocation {
+public interface ILocation {
+
+    void louer(Vehicule vehicule);
+    void rendre(Vehicule vehicule);
+    List<Vehicule> getVehiculesLoues();
+    List<Vehicule> getVehiculesDisponibles();
 }
