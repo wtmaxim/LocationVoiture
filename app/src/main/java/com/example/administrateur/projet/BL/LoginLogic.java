7032f84f01dc6e71e5c45683b7e7f1346e424e26
chatellierclement <chatellierclement44@gmail.com>
2018-04-12 16:25:25 +0200

    bo to json

--- app/src/main/java/com/example/administrateur/projet/BL/LoginLogic.java
+++ app/src/main/java/com/example/administrateur/projet/BL/LoginLogic.java
@@ -3,6 +3,9 @@ package com.example.administrateur.projet.BL;
 import com.example.administrateur.projet.BO.Utilisateur;
 import com.example.administrateur.projet.DAL.UtilisateurEngine;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginLogic {
     private static UtilisateurEngine utilisateurEngine;
 
@@ -16,5 +19,17 @@ public class LoginLogic {
 
     public static Utilisateur getUtilisateurById(int id) {
         return utilisateurEngine.getUtilisateurById(id);
+    }
+
+    public static JSONObject boToJson(Utilisateur utilisateur) {
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+            jsonObject.put("mail", utilisateur.getNom());
+            jsonObject.put("password", utilisateur.getMotDePasse());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
     }
 }
--- app/src/main/java/com/example/administrateur/projet/BL/VehiculeLogic.java
+++ app/src/main/java/com/example/administrateur/projet/BL/VehiculeLogic.java
@@ -4,6 +4,12 @@ import com.example.administrateur.projet.BO.Agence;
 import com.example.administrateur.projet.BO.Vehicule;
 import com.example.administrateur.projet.DAL.VehiculeEngine;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
 /**
  * Created by Administrateur on 12/04/2018.
  */
@@ -20,4 +26,22 @@ public class VehiculeLogic {
         vehiculeEngine.setVehicules();
     }
 
+    public Vehicule jsonToBo(JSONObject jsonObject) {
+        Vehicule vehicule = null;
+        try {
+            vehicule = new Vehicule();
+            vehicule.setId(Integer.parseInt(jsonObject.getString("vehiculeID")));
+            vehicule.setLibelle("Truc");
+            vehicule.setNbPlaces(Integer.parseInt(jsonObject.getString("nbPlaces")));
+            vehicule.setDureeMin(Float.parseFloat(jsonObject.getString("locationMin")));
+            vehicule.setDureeMax(Float.parseFloat(jsonObject.getString("locationMax")));
+            vehicule.setTariffMin(Float.parseFloat(jsonObject.getString("tarifMin")));
+            vehicule.setTarifMax(Float.parseFloat(jsonObject.getString("tarifMax")));
+            vehicule.setEstLoue(Boolean.parseBoolean(jsonObject.getString("isDisponible")));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return vehicule;
+    }
 }
